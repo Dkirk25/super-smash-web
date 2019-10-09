a5f9170f67bd67250a0407d6fd208e00c12fc47e
austinStotts <austintstotts@gmail.com>
2019-10-09 14:37:06 -0500

    npm and react setup

---
+++ app/components/root.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Root extends Component {
+  constructor () {
+    super();
+
+  }
+
+  render () {
+    return (
+      <div>{'HI :)'}</div>
+    )
+  }
+}
+
+export default Root;
