043ad871465e0b883aadb83edf1adcdc66b5d89c
austinStotts <austintstotts@gmail.com>
2019-10-21 16:33:38 -0500

    character styles

--- app/components/character.js
+++ app/components/character.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
-
 const Character = ({ name, select, primary, secondary }) => {
   return (
     <div className={`character-img-wrapper ${primary === name ? "primary" : secondary === name ? "secondary" : null}`} onClick={_=>select(name)}>
-      <img className="character-img" src={`https://super-smash-ranking.s3.amazonaws.com/${name}.png`}></img>
+      <img className={`character-img ${name}`} src={`https://super-smash-ranking.s3.amazonaws.com/${name}.png`}></img>
       <p className="character-name">{`${name.toUpperCase()}`}</p>
     </div>
   )
