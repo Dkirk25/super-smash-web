dbd52480b8008999e27bfb3ba34a447a4eee1540
Miguel Caballero <mcabs3@gmail.com>
2020-06-01 14:17:06 -0500

    column and columns updates and stories

--- src/components/bulma/layout/Columns.tsx
+++ src/components/bulma/layout/Columns.tsx
@@ -7,26 +7,28 @@ export interface ColumnsProps
   extends BulmaComponentProps<
     React.HTMLAttributes<HTMLDivElement>,
     {
-      gapless?: boolean;
-      multiline?: boolean;
+      isVariable?: boolean;
+      isGapless?: boolean;
+      isMultiline?: boolean;
       isCentered?: boolean;
       hasAddons?: boolean;
-      mobile?: boolean;
-      desktop?: boolean;
+      isMobile?: boolean;
+      isDesktop?: boolean;
     },
     "color"
   > {}
 
 export const Columns: React.FC<ColumnsProps> = (props) => {
   const {
-    mobile,
+    isMobile,
     isCentered,
-    desktop,
+    isDesktop,
     children,
     className,
     hasAddons,
-    gapless,
-    multiline,
+    isGapless,
+    isMultiline,
+    isVariable,
     ...rest
   } = props;
 
@@ -38,11 +40,12 @@ export const Columns: React.FC<ColumnsProps> = (props) => {
         bulma,
         {
           "has-addons": hasAddons,
-          "is-gapless": gapless,
-          "is-multiline": multiline,
-          "is-mobile": mobile,
-          "is-desktop": desktop,
+          "is-gapless": isGapless,
+          "is-multiline": isMultiline,
+          "is-mobile": isMobile,
+          "is-desktop": isDesktop,
           "is-centered": isCentered,
+          "is-variable": isVariable,
         },
         className
       )}
--- src/stories/Column.stories.tsx
+++ src/stories/Column.stories.tsx
@@ -7,31 +7,173 @@ import {
   Section,
   Title,
   Columns,
-  Box
+  Box,
 } from "../components/bulma";
 
 export default {
   title: "Column",
-  component: Column
+  component: Column,
 };
 
-export const Basic = () => (
+export const basic = () => (
   <Section>
     <Container>
       <Columns>
-        <Column spanSize={3}>
+        <Column>
           <Box>
             <Title>One</Title>
           </Box>
         </Column>
-        <Column spanSize="one-third">
+        <Column>
           <Box>
             <Title>Two</Title>
           </Box>
         </Column>
-        <Column spanOffset={1}>
+        <Column>
           <Box>
             <Title>Three</Title>
+          </Box>
+        </Column>
+        <Column>
+          <Box>
+            <Title>Four</Title>
+          </Box>
+        </Column>
+      </Columns>
+    </Container>
+  </Section>
+);
+
+export const Sizes = () => (
+  <Section>
+    <Container>
+      {[
+        "full",
+        "four-fifths",
+        "three-quarters",
+        "two-thirds",
+        "three-fifths",
+        "half",
+        "two-fifths",
+        "one-third",
+        "one-quarter",
+        "one-fifth",
+      ].map((span: any) => (
+        <Columns>
+          <Column spanSize={span}>
+            <Box>
+              <Title>{span}</Title>
+            </Box>
+          </Column>
+          {span !== "full" && (
+            <Column>
+              <Box>
+                <Title>Auto</Title>
+              </Box>
+            </Column>
+          )}
+          {span !== "full" && (
+            <Column>
+              <Box>
+                <Title>Auto</Title>
+              </Box>
+            </Column>
+          )}
+        </Columns>
+      ))}
+    </Container>
+  </Section>
+);
+
+export const twelveColumn = () => (
+  <Section>
+    <Container>
+      {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((i: any) => (
+        <Columns>
+          <Column spanSize={i}>
+            <Box>
+              <Title>{i}</Title>
+            </Box>
+          </Column>
+          {i !== 12 && (
+            <Column>
+              <Box>
+                <Title>Auto</Title>
+              </Box>
+            </Column>
+          )}
+        </Columns>
+      ))}
+    </Container>
+  </Section>
+);
+
+export const offset = () => (
+  <Section>
+    <Container>
+      {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((i: any) => (
+        <Columns>
+          <Column spanOffset={i}>
+            <Box>
+              <Title>{i}</Title>
+            </Box>
+          </Column>
+          {i < 11 && (
+            <Column>
+              <Box>
+                <Title>Auto</Title>
+              </Box>
+            </Column>
+          )}
+        </Columns>
+      ))}
+    </Container>
+  </Section>
+);
+
+export const narrow = () => (
+  <Section>
+    <Container>
+      <Columns>
+        <Column isNarrow style={{ width: 200 }}>
+          <Box>
+            <Title>Narrow Column</Title>
+            <p>This column is only 200px wide</p>
+          </Box>
+        </Column>
+        <Column>
+          <Box>
+            <Title>Auto</Title>
+            <p>this column will take up the remaining space</p>
+          </Box>
+        </Column>
+      </Columns>
+    </Container>
+  </Section>
+);
+
+export const isMobile = () => (
+  <Section>
+    <Container>
+      <Columns isMobile>
+        <Column>
+          <Box>
+            <Title>One</Title>
+          </Box>
+        </Column>
+        <Column>
+          <Box>
+            <Title>Two</Title>
+          </Box>
+        </Column>
+        <Column>
+          <Box>
+            <Title>Three</Title>
+          </Box>
+        </Column>
+        <Column>
+          <Box>
+            <Title>Four</Title>
           </Box>
         </Column>
       </Columns>
