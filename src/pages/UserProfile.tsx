b445a09709980f56c03229eb65461b768663b83e
Donald Kirk <dkirk9325@gmail.com>
2020-06-04 00:39:54 -0500

    Updated profile.

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -1,6 +1,25 @@
 import React from "react";
 
-import { Container, Box, Column, Content, Columns } from "../components/bulma";
+import {
+  Container,
+  Box,
+  Column,
+  Content,
+  Columns,
+  Title,
+  Navbar,
+  Section,
+  NavbarEnd,
+  NavbarMenu,
+  NavbarStart,
+  NavbarItem,
+  NavbarDropdown,
+  NavbarDivider,
+  NavbarBrand,
+  NavbarBurger,
+  ButtonList,
+  Button,
+} from "../components/bulma";
 import MockUsers from "../helpers/users";
 
 export interface UserProfileProps {}
@@ -12,27 +31,64 @@ const UserProfile: React.FC<UserProfileProps> = (props) => {
   return (
     <Container>
       {/* Row for Photo left side */}
-      <Columns>
-        <Column spanSize="one-fifth">
-          <Box />
-        </Column>
+      <Section>
+        <Columns>
+          <Column spanSize="one-fifth">
+            <Box />
+          </Column>
 
-        {/* Username, how long they've been a member, total points  */}
-        <Column>Hi</Column>
-      </Columns>
+          {/* Username, how long they've been a member, total points  */}
+          <Column>
+            <Title style={{ paddingBottom: "10%" }}>{user.username}</Title>
+            <Columns>
+              <Column spanSize="one-fifth">
+                <Content>
+                  <span style={{ fontWeight: "bold", fontSize: "24px" }}>
+                    Member Since
+                  </span>
+                  <Content>
+                    <span style={{ fontSize: "18px" }}>01/01/2020</span>
+                  </Content>
+                </Content>
+              </Column>
+
+              <Column>
+                <Content>
+                  <span style={{ fontWeight: "bold", fontSize: "24px" }}>
+                    Total Points
+                  </span>
+                  <Content>
+                    <span style={{ fontSize: "18px" }}>500</span>
+                  </Content>
+                </Content>
+              </Column>
+            </Columns>
+          </Column>
+        </Columns>
+      </Section>
+
+      <Section>
+        <Navbar isSpaced>
+          <NavbarMenu>
+            <NavbarStart>
+              <NavbarItem style={{ fontSize: "24px" }} href="#">
+                About Me
+              </NavbarItem>
+              <NavbarItem style={{ fontSize: "24px" }} href="#">
+                Previous Matches
+              </NavbarItem>
+              <NavbarItem style={{ fontSize: "24px" }} href="#">
+                Record
+              </NavbarItem>
+              <NavbarItem style={{ fontSize: "24px" }} href="#">
+                Past History
+              </NavbarItem>
+            </NavbarStart>
+          </NavbarMenu>
+        </Navbar>
+      </Section>
 
       {/* Create naivagation tabs... Maybe have About Me (Contains fighters, any other contact info), Previous matches, Record (wins, losses, total), Previous Season details? */}
-
-      <div className="league-title-wrapper">
-        <p className="league-title">League Leaderboard</p>
-      </div>
-      <div className="league-user-wrapper-main">
-        <div className="league-user-wrapper">
-          <p className="league-header-username">Username</p>
-          <p className="league-header-main">Main</p>
-          <p className="league-header-points">Score</p>
-        </div>
-      </div>
     </Container>
   );
 };
